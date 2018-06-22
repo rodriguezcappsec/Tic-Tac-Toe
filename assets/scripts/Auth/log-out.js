d9777f8c98125b731e042444b133b456d331e55c
Luis <luis.rodriguezcastro31@gmail.com>
2018-06-21 19:18:20 -0700

    working on logOut

---
+++ assets/scripts/Auth/log-out.js
@@ -0,0 +1,26 @@
+$.ajax({
+    url: config.apiUrl + '/sign-out',
+    method: 'DELETE',
+    headers: {
+        Authorization: 'Token token=' + store.user.token
+    }
+})
+
+let logOut = () => {
+    $("#log-Out").on("submit", event => {
+        let serialization = $("#log-In").serializeArray();
+        event.preventDefault();
+        $.ajax({
+            url:"http://tic-tac-toe.wdibos.com/sign-out",
+            method: 'DELETE',
+            headers: {
+                Authorization: 'Token token=' + store.user.token
+            }
+        }).then(() => {
+            alert("user loged out");
+            }).catch(() => {
+          console.log("error loging out user");
+            
+        })
+    });
+};
