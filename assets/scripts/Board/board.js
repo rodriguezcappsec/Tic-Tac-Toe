778fe5dca561edf9a0b610a1c2cb29f46031411a
Luis <luis.rodriguezcastro31@gmail.com>
2018-06-25 20:30:15 -0700

    trying gameOver

--- assets/scripts/Board/board.js
+++ assets/scripts/Board/board.js
@@ -29,6 +29,7 @@ let ticTacToe = () => {
         saveGame(boardStorage, event.target.id, gameOver);
       }
       boardStorage = []; //Clearing the board after user clicks, and the winner is determined
+      gameOver = false;
     });
   };
   //Showing the board based on the user input
@@ -42,131 +43,124 @@ let ticTacToe = () => {
     });
   };
   showBoard();
+};
 
-  let winner = arr => {
-    var hztlWinner = true;
-    let dgnlWinner = true;
-    let secondDgnlWinner = true;
-    let vtclWinner = true;
-    let dimension = Number(document.getElementById("dimension").value);
-    //Horizontal Checking
-    for (var index = 0; index < arr.length; index++) {
-      hztlWinner = arr[index].every(
-        v => v == arr[index][0] && arr[index][0] != " "
-      );
-      if (hztlWinner) {
-        clickCounter = 0;
-        boolSwitcher = false;
-        gameOver = true;
-        setTimeout(() => {
-          document
-            .querySelectorAll("#tictactoe td")
-            .forEach(e => (e.innerHTML = ""));
-          gameOver = false;
-        }, 1000);
-        modalAlert(`The winner is : Player ${arr[index][0]} --> Row ${index + 1}`, "Congratulations!!", "true");
-      }
-      ("");
-    }
-    //Vertical Checking
-    if (hztlWinner === false) {
-      let vtclChecker = [];
-      for (let y = 0; y < arr.length; y++) {
-        for (let x = 0; x < arr.length; x++) {
-          vtclChecker.push(arr[x][y]);
-          if (vtclChecker.length === arr.length) {
-            (vtclWinner = vtclChecker.every(
-              v => v == vtclChecker[0] && vtclChecker[0] !== " "
-            ));
-            if (vtclWinner) {
-              clickCounter = 0;
-              modalAlert(`The winner is : Player ${vtclChecker[0]} --> Column ${y + 1}`, "Congratulations!!", true);
-              boolSwitcher = false;
-              gameOver = true;
-              setTimeout(() => {
-                document
-                  .querySelectorAll("#tictactoe td")
-                  .forEach(e => (e.innerHTML = ""));
-                gameOver = false;
-              }, 1000);
-            }
-          }
-        }
-        vtclChecker = [];
-      }
-    }
-    //Diagonal Checking
-    if (vtclWinner === false) {
-      let dgnlChecker = [];
-      for (let index = 0; index < arr.length; index++) {
-        dgnlChecker.push(arr[index][index]);
-        if (dgnlChecker.length === arr.length) {
-          dgnlWinner = dgnlChecker.every(v => v == dgnlChecker[0] && dgnlChecker[0] !== " ");
-          if (dgnlWinner) {
-            clickCounter = 0;
-            modalAlert(`The winner is : Player ${dgnlChecker[0]} --> Diagonal ${1}`, "Congratulations!!", true);
-            boolSwitcher = false;
-            gameOver = true;
-            setTimeout(() => {
-              document
-                .querySelectorAll("#tictactoe td")
-                .forEach(e => (e.innerHTML = ""));
-              gameOver = false;
-            }, 1000);
-          }
-        }
-      }
-    }
-    //Second diagonal Checking
-    if (dgnlWinner === false) {
-      let secondDgnlChecker = [];
-      let y = arr.length - 1;
-      for (let x = 0; x < arr.length; ++x) {
-        secondDgnlChecker.push(arr[x][y]);
-        --y;
-        if (secondDgnlChecker.length === arr.length) {
-          y = arr.length - 1;
-          (secondDgnlWinner = secondDgnlChecker.every(
-            v => v == secondDgnlChecker[0] && secondDgnlChecker[0] !== " "
-          ));
-          if (secondDgnlWinner) {
-            clickCounter = 0;
-            modalAlert(
-              `The winner is : Player ${secondDgnlChecker[0]} --> Diagonal ${2}`,
-              "Congratulations!!", true
-            );
-            boolSwitcher = false;
-            gameOver = true;
-            setTimeout(() => {
-              document
-                .querySelectorAll("#tictactoe td")
-                .forEach(e => (e.innerHTML = ""));
-              gameOver = false;
-            }, 1000);
-          }
-        }
-      }
-    }
-    if (
-      clickCounter == dimension * dimension &&
-      secondDgnlWinner == false &&
-      dgnlWinner == false &&
-      vtclWinner == false &&
-      dgnlWinner == false
-    ) {
-      gameOver = false;
-      modalAlert("Raw Game", "No winner!");
+let winner = arr => {
+  var hztlWinner = true;
+  let dgnlWinner = true;
+  let secondDgnlWinner = true;
+  let vtclWinner = true;
+  let dimension = Number(document.getElementById("dimension").value);
+  //Horizontal Checking
+  for (var index = 0; index < arr.length; index++) {
+    hztlWinner = arr[index].every(
+      v => v == arr[index][0] && arr[index][0] != " "
+    );
+    if (hztlWinner) {
       clickCounter = 0;
+      boolSwitcher = false;
+      gameOver = true;
       setTimeout(() => {
         document
           .querySelectorAll("#tictactoe td")
           .forEach(e => (e.innerHTML = ""));
-        gameOver = false;
       }, 1000);
+      modalAlert(`The winner is : Player ${arr[index][0]} --> Row ${index + 1}`, "Congratulations!!", "true");
     }
-  };
+    ("");
+  }
+  //Vertical Checking
+  if (hztlWinner === false) {
+    let vtclChecker = [];
+    for (let y = 0; y < arr.length; y++) {
+      for (let x = 0; x < arr.length; x++) {
+        vtclChecker.push(arr[x][y]);
+        if (vtclChecker.length === arr.length) {
+          (vtclWinner = vtclChecker.every(
+            v => v == vtclChecker[0] && vtclChecker[0] !== " "
+          ));
+          if (vtclWinner) {
+            clickCounter = 0;
+            modalAlert(`The winner is : Player ${vtclChecker[0]} --> Column ${y + 1}`, "Congratulations!!", true);
+            boolSwitcher = false;
+            gameOver = true;
+            setTimeout(() => {
+              document
+                .querySelectorAll("#tictactoe td")
+                .forEach(e => (e.innerHTML = ""));
+            }, 1000);
+          }
+        }
+      }
+      vtclChecker = [];
+    }
+  }
+  //Diagonal Checking
+  if (vtclWinner === false) {
+    let dgnlChecker = [];
+    for (let index = 0; index < arr.length; index++) {
+      dgnlChecker.push(arr[index][index]);
+      if (dgnlChecker.length === arr.length) {
+        dgnlWinner = dgnlChecker.every(v => v == dgnlChecker[0] && dgnlChecker[0] !== " ");
+        if (dgnlWinner) {
+          clickCounter = 0;
+          modalAlert(`The winner is : Player ${dgnlChecker[0]} --> Diagonal ${1}`, "Congratulations!!", true);
+          boolSwitcher = false;
+          gameOver = true;
+          setTimeout(() => {
+            document
+              .querySelectorAll("#tictactoe td")
+              .forEach(e => (e.innerHTML = ""));
+          }, 1000);
+        }
+      }
+    }
+  }
+  //Second diagonal Checking
+  if (dgnlWinner === false) {
+    let secondDgnlChecker = [];
+    let y = arr.length - 1;
+    for (let x = 0; x < arr.length; ++x) {
+      secondDgnlChecker.push(arr[x][y]);
+      --y;
+      if (secondDgnlChecker.length === arr.length) {
+        y = arr.length - 1;
+        (secondDgnlWinner = secondDgnlChecker.every(
+          v => v == secondDgnlChecker[0] && secondDgnlChecker[0] !== " "
+        ));
+        if (secondDgnlWinner) {
+          clickCounter = 0;
+          modalAlert(
+            `The winner is : Player ${secondDgnlChecker[0]} --> Diagonal ${2}`,
+            "Congratulations!!", true
+          );
+          boolSwitcher = false;
+          gameOver = true;
+          setTimeout(() => {
+            document
+              .querySelectorAll("#tictactoe td")
+              .forEach(e => (e.innerHTML = ""));
+          }, 1000);
+        }
+      }
+    }
+  }
+  if (
+    clickCounter == dimension * dimension &&
+    secondDgnlWinner == false &&
+    dgnlWinner == false &&
+    vtclWinner == false &&
+    dgnlWinner == false
+  ) {
+    gameOver = false;
+    modalAlert("Raw Game", "No winner!");
+    clickCounter = 0;
+    setTimeout(() => {
+      document
+        .querySelectorAll("#tictactoe td")
+        .forEach(e => (e.innerHTML = ""));
+    }, 1000);
+  }
 };
-
-
 
 export default ticTacToe;
